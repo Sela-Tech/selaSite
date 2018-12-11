2c1fb8faff64f0a93836f5987fe21246540e03be
Dotun <oludotunlonge@gmail.com>
2018-12-11 13:24:35 +0100

    fixed

--- static.config.js
+++ static.config.js
@@ -71,7 +71,7 @@ function shuffle(array) {
 export default {
   extractCssChunks: true,
   inlineCss: true,
-  siteRoot: "http://sela-labs.co/",
+  siteRoot: "https://sela-labs.co/",
   webpack: (config, { stage }) => {
     if (stage === "prod") {
       config.entry = ["babel-polyfill", config.entry];
