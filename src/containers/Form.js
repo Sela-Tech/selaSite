016e4c61a3841530470ad39d4b31f88fc881adae
Dotun <oludotunlonge@gmail.com>
2019-01-08 16:50:18 +0100

    iframe fix

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -7,7 +7,7 @@ export default  ()=>{
  return <React.Fragment>
       <nav/>
   <Iframe 
-        url="https://docs.google.com/forms/d/e/1FAIpQLSdvHwdkz1wVvA5FfDGs99N1F81GSg3TAhVGT9ZOHFa31LsPRA/viewform?embedded=true" 
+        url="https://docs.google.com/forms/d/e/1FAIpQLSdvHwdkz1wVvA5FfDGs99N1F81GSg3TAhVGT9ZOHFa31LsPRA/viewform" 
     //  url="https://docs.google.com/forms/d/e/1FAIpQLSeb3sQFtYbQzHM3m3VU_9byeDnjwhG19gYUalJSFXpZIumI3Q/viewform"
          width="100vw"
          height="100vh"
