f8acff1cbf7f37b4892e9787414effb07924f53f
Dotun Longe <oludotunlonge@gmail.com>
2019-04-08 17:30:24 +0100

    updates routes

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export default {
-    projects_route: "https://sela-test.now.sh",
-    login_route: "https://sela-test.now.sh/signin",
-    signup_route: "https://sela-test.now.sh/signup",
-    twitter:"#",
-    instagram:"#",
-    youtube:"#",
-    medium:"#",
+    projects_route: "https://app.sela-labs.co",
+    login_route: "https://app.sela-labs.co/signin",
+    signup_route: "https://app.sela-labs.co/signup",
+    twitter:"https://twitter.com/sela_labs?lang=en",
+    instagram:"https://www.instagram.com/sela/",
+    youtube:"https://www.youtube.com/channel/UC0gmyPpmWLMmJeSsgbCizNg/",
+    medium:"https://medium.com/sela-labs",
     whitepaper: "https://docs.wixstatic.com/ugd/96d2bc_2d643e24d9464ba2aeb6f5e6402a5d4e.pdf?dn=Sela+WhitePaper.pdf"
 }
